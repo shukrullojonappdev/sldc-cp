4068d798e77bfa21507d545ea1a85e96863b020a
shukrullojonappdev <shukrullojon.dev@gmail.com>
2022-04-17 16:10:48 +0500

    asdgasg

--- App.tsx
+++ App.tsx
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+
+import 'react-native-gesture-handler'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+
+import Ionicons from '@expo/vector-icons/Ionicons'
+
+import HomeScreen from './screens/HomeScreen'
+import AboutScreen from './screens/AboutScreen'
+
+function DetailsScreen({navigation}: {navigation: any}) {
+	return (
+		<View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+			<Text>Details Screen</Text>
+			<Button title='Go back' onPress={() => navigation.goBack()} />
+		</View>
+	)
+}
+
+const Drawer = createDrawerNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+			<Drawer.Navigator initialRouteName='Home'>
+				<Drawer.Screen name='Home' component={HomeScreen} 
+					options={{
+						title: 'Home',
+						drawerIcon: ({focused, size}) => (
+							<Ionicons
+								name='md-home'
+								size={size}
+								color={focused ? '#7cc' : '#ccc'}
+							/>
+						)
+					}}
+				/>
+				<Drawer.Screen name='Details' component={DetailsScreen}
+					options={{
+						title: 'Details',
+						drawerIcon: ({focused, size}) => (
+							<Ionicons
+								name='md-documents'
+								size={size}
+								color={focused ? '#7cc' : '#ccc'}
+							/>
+						)
+					}}
+				/>
+				<Drawer.Screen name='About' component={AboutScreen} 
+					options={{
+						title: 'About',
+						drawerIcon: ({focused, size}) => (
+							<Ionicons
+								name='md-alarm'
+								size={size}
+								color={focused ? '#7cc' : '#ccc'}
+							/>
+						)
+					}}
+				/>
+				</Drawer.Navigator>
+		</NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
